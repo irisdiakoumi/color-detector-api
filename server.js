0f311648f6115d92c126363338fb3212fe2a6bd4
Iris <iridiumcodes@gmail.com>
2021-01-18 22:30:22 +0200

    API initial design endpoints

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send("this is working")
+} )
+
+/* ROUTES
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = new user
+/profile/:userId --> GET = user
+/palettes --> PUT = updated user
+
+*/
+
 app.listen(3000, () => {
   console.log('app is running on port 3000');
 });
