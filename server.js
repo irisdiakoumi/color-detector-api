84d5321e386409f3871589723e09d53e1c0ad1d7
Iris <iridiumcodes@gmail.com>
2021-02-02 23:34:51 +0200

    registration controller

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import knex from 'knex';
 import bcrypt from 'bcryptjs';
 
+import {handleRegister} from './controllers/register.js';
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -30,34 +32,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-  const {name, email, password} = req.body;
-  console.log(password);
-  const hash = bcrypt.hashSync(password, 10);
-  db.transaction((trx) => {
-    //tranasctions in knex
-    trx
-      .insert({
-        hash: hash,
-        email: email,
-      })
-      .into('login')
-      .returning('email')
-      .then((loginEmail) => {
-        return trx('users')
-          .returning('*') //knex method
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date(),
-          })
-          .then((user) => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.commit) //VERY important to actually commit the additions and rollback in case anything fails
-      .catch(trx.rollback);
-  }).catch((err) => res.status(400).json('unable to register'));
-  // res.json(database.users[database.users.length - 1]); //shows the latest user registered
+  handleRegister(req, res, db, bcrypt); //dependency injection
 });
 
 app.post('/signin', (req, res) => {
