0a12c86966da84d44796b35ed73f99f4e2687c9f
Iris <iridiumcodes@gmail.com>
2021-02-05 20:25:41 +0200

    update database host variable

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import {handleApiCall, handlePalettesSave} from './controllers/palettes.js';
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   },
 });
